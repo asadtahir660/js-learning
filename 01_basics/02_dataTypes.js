514a79bdbaa7bbd365e5c83afe0c013692e3531d
Asad Tahir <asad.tahir@innoversetech.org>
2025-02-18 14:25:15 +0000

    02_dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+// "use strict"; // treat all JS code as newer version
+
+let = "Asad";
+let age = 28;
+let isLoggedIn = true;
+
+// Number => 2 3 5 
+// bigint
+// string => "text"
+// bollean => True/False 
+// null => standalone value
+// undefined 
+// symbol => Unique 
+
+
+// object
+
+
+// Null type is object
+
+console.log(typeof null);
