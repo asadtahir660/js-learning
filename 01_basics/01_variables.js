f12268319a1a212cdba82e48410b2e10280d9a4d
Asad Tahir <asad.tahir@innoversetech.org>
2025-02-18 13:35:48 +0000

    01_Variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 144
+let accountEmail= "demo@gmail.com"
+var accountPassword = "1234"
+accountCity = "Lahore"
+
+accountEmail= "asad@gmail.com"
+accountPassword= "223344"
+accountCity = "isalamabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
