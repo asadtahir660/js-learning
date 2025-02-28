e3d3012c6c14a40f7ab88889556d03a934fe0ba4
Asad Tahir <asad.tahir@innoversetech.org>
2025-02-28 12:59:46 +0000

    Learned Conversion Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// let data = null;
+
+// console.log(typeof Number(data));
+
+// let dataValue = true;
+
+// console.log(typeof dataValue);
+
+// let bolleanValue = 1;
+// let bolleanValue = "Asad";
+// let bolleanValue = "";
+
+// console.log(Boolean(bolleanValue))
+
+let someNumber = 33;
+ let stringNumberValue = String(someNumber);
+console.log(stringNumberValue);
+console.log(typeof stringNumberValue);
+
+
